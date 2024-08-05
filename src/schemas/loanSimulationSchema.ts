24bec813b97c8dbe4a3638228d49c284cee29de8
Pedro <pedrohlucena413@gmail.com>
2024-08-05 20:30:57 -0300

    refactor: add error messages modularization for form fields validation

--- src/schemas/loanSimulationSchema.ts
+++ src/schemas/loanSimulationSchema.ts
@@ -1,30 +1,36 @@
 import { birthDateRegex, cpfRegex } from 'regex'
 import { z } from 'zod'
 
+enum ERROR_MESSAGES {
+  INVALID_CPF = 'O CPF deve ser informado no formato 000.000.000-00',
+  INVALID_BIRTH_DATE = 'A data de nascimento deve ser informada no formato 00/00/0000',
+  REQUIRED_LOAN_VALUE = 'O valor do empréstimo é obrigatório',
+  LOAN_VALUE_MUST_BE_NUMBER = 'O valor do empréstimo deve ser um número',
+  LOAN_VALUE_MUST_BE_POSITIVE = 'O valor do empréstimo deve ser positivo',
+  MIN_LOAN_VALUE = 'O valor mínimo do empréstimo é R$ 50.000,00',
+  REQUIRED_INSTALLMENT_VALUE = 'O valor da parcela é obrigatório',
+  INSTALLMENT_VALUE_MUST_BE_NUMBER = 'O valor da parcela deve ser um número',
+  INSTALLMENT_VALUE_MUST_BE_POSITIVE = 'O valor da parcela deve ser positivo',
+}
+
 export const loanSimulationSchema = z.object({
-  cpf: z
-    .string()
-    .regex(cpfRegex, 'O CPF deve ser informado no formato 000.000.000-00')
-    .length(14),
+  cpf: z.string().regex(cpfRegex, ERROR_MESSAGES.INVALID_CPF).length(14),
   uf: z.string(),
   birthDate: z
     .string()
-    .regex(
-      birthDateRegex,
-      'A data de nascimento deve ser informada no formato 00/00/0000',
-    )
+    .regex(birthDateRegex, ERROR_MESSAGES.INVALID_BIRTH_DATE)
     .length(10),
   loanValue: z.coerce
     .number({
-      required_error: 'O valor do emprestimo é obrigatório',
-      invalid_type_error: 'O valor do emprestimo deve ser um número',
+      required_error: ERROR_MESSAGES.REQUIRED_LOAN_VALUE,
+      invalid_type_error: ERROR_MESSAGES.LOAN_VALUE_MUST_BE_NUMBER,
     })
-    .positive('O valor do emprestimo deve ser positivo')
-    .min(50000, 'O valor mínimo do emprestimo é R$ 50.000,00'),
+    .positive(ERROR_MESSAGES.LOAN_VALUE_MUST_BE_POSITIVE)
+    .min(50000, ERROR_MESSAGES.MIN_LOAN_VALUE),
   installmentValue: z.coerce
     .number({
-      required_error: 'O valor da parcela é obrigatório',
-      invalid_type_error: 'O valor da parcela deve ser um número',
+      required_error: ERROR_MESSAGES.REQUIRED_INSTALLMENT_VALUE,
+      invalid_type_error: ERROR_MESSAGES.INSTALLMENT_VALUE_MUST_BE_NUMBER,
     })
-    .positive('O valor da parcela deve ser positivo'),
+    .positive(ERROR_MESSAGES.INSTALLMENT_VALUE_MUST_BE_POSITIVE),
 })
